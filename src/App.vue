48a0af82374d2f5e760da19e84d72ea3b4c84914
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-03-29 18:30:04 +0100

    ajout de la parti questions mais reste des trucs à faire

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import QuestionnaireItem from './components/QuestionnaireItem.vue';
 
 let data = {
   questionnaires: [
-    { id: 1, name: 'Faire les courses', uri: '' },
-    { id: 2, name: 'Apprendre REST', uri : '' }
+    { id: 1, name: 'Faire les courses', uri: '', questions: [] },
+    { id: 2, name: 'Apprendre REST', uri: '', questions: [] }
   ],
   title: 'Mes questionnaires',
   newItem: ''
@@ -20,7 +20,8 @@ export default {
       let questionnaire = {
         id: this.questionnaires.length + 1,
         name: name,
-        uri: 'http://localhost:5000/quiz/api/v1.0/questionnaires' + (this.questionnaires.length + 1)
+        uri: 'http://localhost:5000/quiz/api/v1.0/questionnaires' + (this.questionnaires.length + 1),
+        questions: []
       }
       const promise = this.saveQuestionnaire(questionnaire);
       console.log(promise);
@@ -28,9 +29,7 @@ export default {
         console.log(resultat);
         this.questionnaires.push(questionnaire);
         this.newItem = '';
-      }
-      );
-
+      });
     },
     saveQuestionnaire: function (questionnaire) {
       return fetch('http://localhost:5000/quiz/api/v1.0/questionnaires', {
@@ -50,6 +49,22 @@ export default {
           console.error('Error:', error);
         });
     },
+    replaceItem: function (item) {
+      let name = item.name.trim();
+      let questionnaire = {
+        id: item.id,
+        name: name,
+        uri: 'http://localhost:5000/quiz/api/v1.0/questionnaires' + item.id,
+        questions: item.questions
+      }
+      const promise = this.saveQuestionnaire(questionnaire);
+      console.log(promise);
+      promise.then((resultat) => {
+        console.log(resultat);
+        this.questionnaires.splice(this.questionnaires.indexOf(item), 1, questionnaire);
+        this.newItem = '';
+      });
+    },
     removeItem: function (id) {
       return fetch("http://localhost:5000/quiz/api/v1.0/questionnaires/" + id, {
         method: 'DELETE',
@@ -62,7 +77,50 @@ export default {
         .catch((error) => {
           console.error('Error:', error);
         });
-
+    },
+    getQuestionsForQuestionnaire: function(questionnaireId) {
+      return fetch(`http://localhost:5000/quiz/api/v1.0/questionnaires/${questionnaireId}/questions`)
+        .then(response => response.json())
+        .then(data => {
+          console.log('Questions:', data);
+          const questionnaire = this.questionnaires.find(q => q.id === questionnaireId);
+          if (questionnaire) {
+            questionnaire.questions = data;
+          }
+          return data;
+        })
+        .catch((error) => {
+          console.error('Error fetching questions:', error);
+        });
+    },
+    addQuestionToQuestionnaire: function(questionnaireId, question) {
+      const questionData = {
+        title: question.title,
+        questionType: question.questionType,
+        questionnaire_id: questionnaireId
+      };
+      
+      return fetch(`http://localhost:5000/quiz/api/v1.0/questionnaires/${questionnaireId}/questions`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(questionData)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Question added:', data);
+          const questionnaire = this.questionnaires.find(q => q.id === questionnaireId);
+          if (questionnaire) {
+            questionnaire.questions.push(data);
+          }
+          return data;
+        })
+        .catch((error) => {
+          console.error('Error adding question:', error);
+        });
+    }
   },
   mounted() {
     fetch('http://localhost:5000/quiz/api/v1.0/questionnaires')
@@ -70,10 +128,13 @@ export default {
       .then(data => {
         console.log(data);
         this.questionnaires = data;
+        this.questionnaires.forEach(questionnaire => {
+          this.getQuestionsForQuestionnaire(questionnaire.id);
+        });
       });
   },
   components: { QuestionnaireItem }
-}}
+}
 </script>
 
 <template>
@@ -83,14 +144,15 @@ export default {
     <h2>{{ title }}</h2>
     
     <ol>
-      <li v-for="questionnaire in questionnaires"  :key="questionnaire.id">
+      <li v-for="questionnaire in questionnaires" :key="questionnaire.id">
         <div class="checkbox">
           <label>
-             {{ questionnaire.name }}
+             {{ questionnaire.name }} ({{ questionnaire.questions ? questionnaire.questions.length : 0 }} questions)
             <QuestionnaireItem
-              :todo="questionnaire"
+              :questionnaire="questionnaire"
               @remove="removeItem"
               @replace="replaceItem"
+              @add-question="addQuestionToQuestionnaire"
             ></QuestionnaireItem>
           </label>
         </div>
--- src/components/QuestionnaireItem.vue
+++ src/components/QuestionnaireItem.vue
@@ -3,37 +3,86 @@ export default {
   props: {
     questionnaire: Object
   },
+  data() {
+    return {
+      showQuestions: false,
+      newQuestionTitle: '',
+      newQuestionType: 'text'
+    }
+  },
   methods: {
     suppr() {
       this.$emit('remove', this.questionnaire.id);
     },
     changerContenu() {
-      const newText = prompt("Entrez le nouveau texte :");
+      let newText = prompt("Entrez le nouveau nom du questionnaire:", this.questionnaire.name);
       if (newText && newText.trim()) {
-        this.$emit('replace', { id: this.todo.id, text: newText });
+        this.$emit('replace', { id: this.questionnaire.id, name: newText });
       }
     },
-    async fetchData() {
-        this.todos = null
-        const res = await fetch(
-            `http://localhost:5000/quiz/api/v1.0/questionnaires`
-        )
-        this.todos = await res.json()
+    afficherQuestions() {
+      this.showQuestions = !this.showQuestions;
     },
-    mounted() {
-            this.fetchData()
+    addQuestion() {
+      if (this.newQuestionTitle.trim()) {
+        const newQuestion = {
+          title: this.newQuestionTitle,
+          questionType: this.newQuestionType,
+          questionnaire_id: this.questionnaire.id
+        };
+        this.$emit('add-question', this.questionnaire.id, newQuestion);
+        this.newQuestionTitle = '';
+      }
     }
   }
 }
 </script>
 
 <template>
-  <input type="button"
-         class="btn btn-danger"
-         value="Supprimer"
-         @click="suppr">
-  <input type="button"
-         class="btn btn-replace"
-         value="Remplacer"
-         @click="changerContenu">
+  <div>
+    <input type="button"
+           class="btn btn-danger"
+           value="Supprimer"
+           @click="suppr">
+    <input type="button"
+           class="btn btn-replace"
+           value="Remplacer"
+           @click="changerContenu">
+    <input type="button"
+           class="btn btn-info"
+           value="Afficher Questions"
+           @click="afficherQuestions">
+           
+    <div v-if="showQuestions" class="mt-3">
+      <h5>Questions:</h5>
+      <ul class="list-group">
+        <li v-if="questionnaire.questions && questionnaire.questions.length === 0" class="list-group-item">
+          Aucune question pour ce questionnaire
+        </li>
+        <li v-for="question in questionnaire.questions" :key="question.id" class="list-group-item">
+          <strong>{{ question.title }}</strong> (Type: {{ question.questionType }})
+        </li>
+      </ul>
+      
+      <div class="mt-3">
+        <h6>Ajouter une question:</h6>
+        <div class="input-group mb-3">
+          <input 
+            v-model="newQuestionTitle" 
+            @keyup.enter="addQuestion" 
+            placeholder="Titre de la question" 
+            type="text" 
+            class="form-control">
+          
+          <select v-model="newQuestionType" class="form-select">
+            <option value="text">Texte</option>
+            <option value="choice">Choix multiple</option>
+            <option value="boolean">Oui/Non</option>
+          </select>
+          
+          <button @click="addQuestion" class="btn btn-success" type="button">Ajouter</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
